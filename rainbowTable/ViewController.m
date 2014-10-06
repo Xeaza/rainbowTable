5a04f13644f97368c95864dc5f9827c86b0d3326
xeaza <taylorwrightsanson@gmail.com>
2014-10-06 12:33:29 -0500

    Added cells and made a random color method to set the color for each cell

--- rainbowTable/ViewController.m
+++ rainbowTable/ViewController.m
@@ -8,20 +8,45 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 
+@property int colorCounter;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.colorCounter = 0;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 500;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"colorCellId"];
+
+    UIColor *color = [self getColor:indexPath.row];
+    cell.backgroundColor = color;
+        return cell;
+}
+
+- (UIColor *)getColor: (NSInteger)indexPath {
+    UIColor *color = [[UIColor alloc] init];
+
+    float random1 = arc4random() % 255;
+    float random2 = arc4random() % 255;
+
+    for (float i = 0.0; i <= indexPath; i++) {
+        color = [UIColor colorWithRed:(i/255.0) green:(random1/255.0) blue:(random2/255.0) alpha:1.0];
+    }
+    return color;
 }
 
 @end
