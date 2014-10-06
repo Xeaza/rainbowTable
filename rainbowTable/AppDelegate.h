f008da3395837f0f68ed776492c9d69f3ad4476c
xeaza <taylorwrightsanson@gmail.com>
2014-10-06 11:24:44 -0500

    Initial Commit

---
+++ rainbowTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rainbowTable
+//
+//  Created by Taylor Wright-Sanson on 10/6/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rainbowTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rainbowTable
+//
+//  Created by Taylor Wright-Sanson on 10/6/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
